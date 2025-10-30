1a0aa5f1618aec138d445cbb08a7394bbc96a7c9
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-10-30 17:43:38 +1100

    LC-merge-sorted-array [Runtime: 0 ms; Memory: 54.6 MB]

--- sorting/merge-sorted-array.js
+++ sorting/merge-sorted-array.js
@@ -6,24 +6,39 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let temp = [];
-    let i = 0, j = 0;
+
+/*
+Use two pointers i and j to iterate through nums1 and nums2.
+At each step, compare elements from both arrays and push the smaller (or equal) one into a temporary array temp.
+After one array is fully traversed, append the remaining elements from the other array into temp.
+Finally, copy all merged elements from temp back into nums1.
+ */
+
+    let temp = []
+    let i = 0, j = 0
 
     while (i < m && j < n) {
         if (nums1[i] <= nums2[j]) {
-            temp.push(nums1[i]);
-            i++;
+            temp.push(nums1[i])
+            i++
         } else {
-            temp.push(nums2[j]);
-            j++;
+            temp.push(nums2[j])
+            j++
         }
     }
 
-    while (i < m) temp.push(nums1[i++]);
-    while (j < n) temp.push(nums2[j++]);
-    
-    for (let k = 0; k < temp.length; k++) {
-        nums1[k] = temp[k];
+    while (j < n) {
+        temp.push(nums2[j])
+        j++
     }
 
+    while (i < m) {
+        temp.push(nums1[i])
+        i++
+    }
+
+    for (let i = 0; i < temp.length; i++) {
+        nums1[i] = temp[i]
+    }
+    
 };
