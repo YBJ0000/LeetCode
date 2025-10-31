6457916c45b47de73f0fb3a3a3ac8656c3508e14
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-10-31 13:08:15 +1100

    LC-best-time-to-buy-and-sell-stock [Runtime: 2 ms; Memory: 65.5 MB]

--- array/best-time-to-buy-and-sell-stock.js
+++ array/best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,36 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let current_max = 0
+
+    let minPrice = Infinity
+    let maxProfit = 0
 
     for (let i = 0; i < prices.length; i++) {
-        for (let j = i + 1; j < prices.length; j++) {
-            if ((prices[j] - prices[i]) > current_max) {
-                current_max = prices[j] - prices[i]
-            }
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice
         }
     }
 
-    return current_max
+    return maxProfit
+
+
+
+    /*
+    Using a double loop will exceed the time limit.
+    */
+
+    // let current_max = 0
+
+    // for (let i = 0; i < prices.length; i++) {
+    //     for (let j = i + 1; j < prices.length; j++) {
+    //         if ((prices[j] - prices[i]) > current_max) {
+    //             current_max = prices[j] - prices[i]
+    //         }
+    //     }
+    // }
+
+    // return current_max
     
 };
