952854587f08741bc7fdc04bee3d88515223209f
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-10-31 12:50:06 +1100

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    current_max = 0
+
+    for (let i = 0; i < prices.length; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            if ((prices[j] - prices[i]) > current_max) {
+                current_max = prices[j] - prices[i]
+            }
+        }
+    }
+
+    return current_max
+    
+};
