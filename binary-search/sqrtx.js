0688684bac11a0c2f0817d013a21d61bf728ea2f
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-10-31 13:25:55 +1100

    LC-sqrtx [Runtime: N/A; Memory: N/A]

---
+++ binary-search/sqrtx.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if (x === 0) return 0
+    if (x === 1) return 1
+
+    for (i = 0; i < x; i++) {
+        if (i * i === x) {
+            return i
+        } else if (i * i > x) {
+            return (i - 1)
+        }
+    }
+};
