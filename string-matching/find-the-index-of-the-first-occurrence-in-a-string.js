30675733883980d57a5908ea3971ab6c9875143f
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-11-01 15:23:46 +1100

    LC-find-the-index-of-the-first-occurrence-in-a-string [Runtime: N/A; Memory: N/A]

--- string-matching/find-the-index-of-the-first-occurrence-in-a-string.js
+++ string-matching/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,15 +4,32 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
+
     for (let i = needle.length - 1; i < haystack.length; i++) {
-        if (needle[needle.length-1] === haystack[i]) {
-            if (needle[needle.length-2] === haystack[i-1]) {
-                if (needle[needle.length-3] === haystack[i-2]) {
-                    return i - needle.length + 1
-                }
+        for (let j = 1; j <= needle.length; j++) {
+            if (needle[needle.length-j] === haystack[i-j+1]) {  // find the pattern
+                return i - needle.length + 1
             }
-        } else {
-            return -1
         }
     }
+
+    return -1
+
+
+
+    // About Syntax:
+    // str[str.length - 1] ✅
+    // str[-1] ❌
+
+    // for (let i = needle.length - 1; i < haystack.length; i++) {
+    //     if (needle[needle.length-1] === haystack[i]) {
+    //         if (needle[needle.length-2] === haystack[i-1]) {
+    //             if (needle[needle.length-3] === haystack[i-2]) {
+    //                 return i - needle.length + 1
+    //             }
+    //         }
+    //     } else {
+    //         return -1
+    //     }
+    // }
 };
