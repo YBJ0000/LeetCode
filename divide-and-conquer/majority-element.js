c89721d8c6f64ef0e685bba9764f0c324cedea7f
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-11-03 16:44:51 +1100

    LC-majority-element [Runtime: 5 ms; Memory: 57.2 MB]

--- divide-and-conquer/majority-element.js
+++ divide-and-conquer/majority-element.js
@@ -3,22 +3,30 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
+    /*
+    Create a dictionary
+    Iterate through the array, store all elements and their counts into the dictionary
+    Create a two variables `maxCount` and `maxKey` to store the maximum count so far and the so far majority element
+    Iterate through the dictionary, if the count of the current element is higher than the `maxCount`, update the `maxCount` and `maxKey`
+    Return the `maxKey` (transfer it into Number)
+    */
+
     let count = {}
 
-    for (let num of nums) {
-        if (!count[num]) {
-            count[num] = 1
+    for (let i = 0; i < nums.length; i++) {
+        if (!count[nums[i]]) {
+            count[nums[i]] = 1
         } else {
-            count[num]++
+            count[nums[i]]++
         }
     }
 
-    let maxValue = -Infinity
+    let maxCount = -Infinity
     let maxKey = null
 
     for (let key in count) {
-        if (count[key] > maxValue) {
-            maxValue = count[key]
+        if (count[key] > maxCount) {
+            maxCount = count[key]
             maxKey = key
         }
     }
