cfb431bfadc5f012d08f7dd19df9eae4e8403b51
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-11-04 17:56:06 +1100

    LC-remove-element [Runtime: 0 ms; Memory: 54.1 MB]

--- two-pointers/remove-element.js
+++ two-pointers/remove-element.js
@@ -6,22 +6,48 @@
 var removeElement = function(nums, val) {
 
     /*
-    Create a temporary array `temp`.
-    Iterate through nums, if the current element is not equal to `val`, push it to `temp`.
-    In the end, copy all the elements from `temp` back into nums1, and return the length of `temp`.
-    Time complexity: O(n)
+    Use two pointers:
+        - `read`: scans through the array
+        - `write`: points to the next position to place a non-`val` element
+    If `nums[read]` is not equal to `val`, write it to `nums[write]` and move `write` forward
+    Otherwise, skip it
+    Return the `write` as the count of valid elements
     */
-    let temp = []
 
-    for (let num of nums) {
-        if (num !== val) {
-            temp.push(num)
+    let write = 0
+    for (let read = 0; read < nums.length; read++) {
+        if (nums[read] !== val) {
+            nums[write] = nums[read]
+            write++
         }
     }
 
-    for (let i = 0; i < temp.length; i++) {
-        nums[i] = temp[i]
-    }
+    return write
 
-    return temp.length
+
+
+
+
+
+
+
+
+    // let p1 = 0, p2 = 0
+
+    // while (p1 < nums.length && p2 < nums.length) {
+    //     if (nums[p2] === val) {
+    //         p2++
+    //     } else {
+    //         if (p1 !== p2) {
+    //             nums[p1] = nums[p2]
+    //             p1++
+    //             p2++
+    //         } else {
+    //             p1++
+    //             p2++
+    //         }
+    //     }
+    // }
+
+    // return p1
 };
