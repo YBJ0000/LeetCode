c26cf35e717c313c6bae62ac651d80d724aa416b
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-11-06 19:42:37 +1100

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

---
+++ trie/longest-common-prefix.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+
+    // find the shortest word and its char count
+    let min_char_count = Infinity
+    for (let i = 0; i < strs.length; i++) {
+        if (strs[i].length < min_char_count) {
+            min_char_count = strs[i].length
+        }
+    }
+
+    let prefix = ""
+
+    for (let i = 0; i < min_char_count; i++) {
+        for (let j = 0; j < strs.length - 1; j++) {
+            if (strs[j][i] !== strs[j+1][i]) {
+                return prefix
+            }
+        }
+        prefix = prefix + strs[0][i]
+    }
+};
