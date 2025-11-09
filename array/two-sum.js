caaca0d0d5eedf4a5aadef78f1a84317671d6a31
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-11-10 00:55:47 +1100

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.js
+++ array/two-sum.js
@@ -4,19 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let result = []
-    let p1 = 0, p2 = nums.length - 1
 
-    while (p1 < nums.length -1) {
-        if (nums[p1] + nums[p2] === target) {
-            result = [p1, p2]
-            return result
-        } else if (p2 - p1 === 1) {
-            p1++
-            p2 = nums.length - 1
-        } else if (p1 < p2) {
-            p2--
-        }
-    }
+
+
+
+    // // use two pointers
+    // let result = []
+    // let p1 = 0, p2 = nums.length - 1
+
+    // while (p1 < nums.length -1) {
+    //     if (nums[p1] + nums[p2] === target) {
+    //         result = [p1, p2]
+    //         return result
+    //     } else if (p2 - p1 === 1) {
+    //         p1++
+    //         p2 = nums.length - 1
+    //     } else if (p1 < p2) {
+    //         p2--
+    //     }
+    // }
 
 };
