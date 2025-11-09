82996d8cfce88e895831bf4432739bb56ab1a8cb
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-11-09 11:43:27 +1100

    LC-length-of-last-word [Runtime: 0 ms; Memory: 53.4 MB]

--- string/length-of-last-word.js
+++ string/length-of-last-word.js
@@ -3,6 +3,14 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
+    /*
+    Set `count` to store the words count of the last word
+    Iterate through the string reversely:
+        - If the current char is not space, count plus 1
+        - If the current char is space and the count is not 0, return count
+    - At the end, return count. (Otherwise, test cases that don't have space will fail)
+    */
+
     let count = 0
 
     for (let i = s.length - 1; i >= 0; i--) {
