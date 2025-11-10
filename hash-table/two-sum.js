c58151c1a4f2584ecbb48e85e5cf8314405b0703
Bingjia Yang <95595902+YBJ0000@users.noreply.github.com>
2025-11-10 16:04:42 +1100

    LC-two-sum [Runtime: 1 ms; Memory: 57.8 MB]

--- hash-table/two-sum.js
+++ hash-table/two-sum.js
@@ -5,23 +5,39 @@
  */
 var twoSum = function(nums, target) {
 
+    const seen = new Map()
 
+    for (let i = 0; i < nums.length; i++) {
+        const need = target - nums[i]
 
-
-    // // use two pointers
-    let result = []
-    let p1 = 0, p2 = nums.length - 1
-
-    while (p1 < nums.length -1) {
-        if (nums[p1] + nums[p2] === target) {
-            result = [p1, p2]
-            return result
-        } else if (p2 - p1 === 1) {
-            p1++
-            p2 = nums.length - 1
-        } else if (p1 < p2) {
-            p2--
+        if (seen.has(need)) {
+            return [seen.get(need), i]
         }
+
+        seen.set(nums[i], i)
     }
+
+    return []
+
+
+
+
+
+
+    // // // use two pointers
+    // let result = []
+    // let p1 = 0, p2 = nums.length - 1
+
+    // while (p1 < nums.length -1) {
+    //     if (nums[p1] + nums[p2] === target) {
+    //         result = [p1, p2]
+    //         return result
+    //     } else if (p2 - p1 === 1) {
+    //         p1++
+    //         p2 = nums.length - 1
+    //     } else if (p1 < p2) {
+    //         p2--
+    //     }
+    // }
 
 };
